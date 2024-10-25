a27b598eeb6a8c441bc9ad4720479aac23fd956b
dhanush prabakaran <dhanushprabakaran18@gmail.com>
2024-10-25 11:18:37 +0530

    updated

--- task2/script.js
+++ task2/script.js
@@ -64,7 +64,7 @@ function datafeed() {
   let quizHTML = "";
   data.forEach((item, index) => {
     quizHTML += `
-            <div class="flex flex-col mb-4">
+            <div class="flex flex-col  mb-4">
                 <label for="q${index + 1}" class="text-2xl text-gray-800 mb-2">
                     ${index + 1}. ${item.question}
                 </label>
